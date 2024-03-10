77e87abe842708b19551bdeb335cd0e3b60a2e99
Hoang Truong <hoangtruong181099@gmail.com>
2024-03-11 06:06:21 +0700

    add solution

---
+++ Exercise_2/main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+    profile: "default"
+    region: var.aws_region
+}
+
+data archive_file "archive" {
+  type        = "zip"
+  output_path = "${path.module}/files/output.zip"
+
+  source {
+    content  = "lambda"
+    filename = "greet_lambda.py"
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws-lambda-function" "greet_lambda" {
+    filename: var.output_archive_name
+    function_name: var.lambda_function_name
+    handler: var.lambda_handler
+
+    source_code_hash = data.archive_file.archive.output_base64sha256
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,2 @@
 # TODO: Define the variable for aws_region
+aws_region="us-east-1"
