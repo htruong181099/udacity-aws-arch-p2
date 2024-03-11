cded619762e1385addef4e75740e76ed503a85a2
Hoang Truong <hoangtruong181099@gmail.com>
2024-03-11 21:09:36 +0700

    Add EC2 instance name for Ex1 & Update estimate cost

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -9,12 +9,19 @@ resource "aws_instance" "Udacity-T2" {
     ami = "ami-0f403e3180720dd7e"
     instance_type = "t2.micro"
     count = 4
+    tags = {
+        Name = "Udacity T2"
+    }
 }
 
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
-resource "aws_instance" "Udacity_M4" {
-    ami = "ami-0f403e3180720dd7e"
-    instance_type = "m4.large"
-    count = 2
-}
+# resource "aws_instance" "Udacity_M4" {
+#     ami = "ami-0f403e3180720dd7e"
+    
+#     instance_type = "m4.large"
+#     count = 2
+#     tags = {
+#         Name = "Udacity M4"
+#     }
+# }
